035dc5d61bca0160225fa0661cb018df4dbdb831
marshall <CortexPE@users.noreply.github.com>
2021-03-19 02:13:46 +0800

    include response code

--- src/CortexPE/DiscordWebhookAPI/task/DiscordWebhookSendTask.php
+++ src/CortexPE/DiscordWebhookAPI/task/DiscordWebhookSendTask.php
@@ -53,14 +53,14 @@ class DiscordWebhookSendTask extends AsyncTask {
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
-		$this->setResult(curl_exec($ch));
+		$this->setResult([curl_exec($ch), curl_getinfo($ch, CURLINFO_RESPONSE_CODE)]);
 		curl_close($ch);
 	}
 
 	public function onCompletion(Server $server){
 		$response = $this->getResult();
-		if($response !== ""){
-			$server->getLogger()->error("[DiscordWebhookAPI] Got error: " . $response);
+		if(!in_array($response[1], [200, 204])){
+			$server->getLogger()->error("[DiscordWebhookAPI] Got error ({$response[1]}): " . $response[0]);
 		}
 	}
 }
